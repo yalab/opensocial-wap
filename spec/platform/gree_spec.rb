38255e525969690ee54280e1eb4c26a8f1d1d590
t1732 <oo1732oo@gmail.com>
2013-07-08 17:16:58 +0900

    Updated mobage2,gree2をそれぞれmobage,greeに変更して利用しないplaformは削除

--- spec/platform/gree_spec.rb
+++ spec/platform/gree_spec.rb
@@ -3,46 +3,37 @@ require 'spec_helper'
 
 describe OpensocialWap::Platform do
   describe "gree" do
-    it "sandbox用に、GREE用の初期化が正しく行えること(セッションOFF)" do
-      c = Rails::Application::Configuration.new
-      OpensocialWap::Platform.gree(c) do
-        consumer_key '(consumer_key)'
-        consumer_secret '(consumer_secret)'
-        sandbox true
-        session false
+    context "gree用の初期化が正しく行えること" do
+      before do
+        @config = Rails::Application::Configuration.new
+        OpensocialWap::Platform.gree(@config) do
+          consumer_key '(consumer_key)'
+          consumer_secret '(consumer_secret)'
+          sandbox true
+          session false
+        end
       end
 
-      c.opensocial_wap.oauth.helper_class.should == OpensocialWap::OAuth::Helpers::BasicHelper
-      c.opensocial_wap.oauth.helper_class.proxy_class.should == OpensocialWap::OAuth::RequestProxy::OAuthRackRequestProxy
-      c.opensocial_wap.oauth.helper_class.consumer_key.should == '(consumer_key)'
-      c.opensocial_wap.oauth.helper_class.consumer_secret.should == '(consumer_secret)'
-      c.opensocial_wap.oauth.helper_class.api_endpoint.should == 'http://os-sb.gree.jp/api/rest/'
-      c.opensocial_wap.url.container_host == 'mgadget-sb.gree.jp'
-      c.opensocial_wap.url.default.should == {:format => :query, :params => { :guid => 'ON' }}
-      c.opensocial_wap.url.redirect.should == {:format => :local}
-      c.opensocial_wap.url.public_path.should == {:format => :local}
-      c.opensocial_wap.session_id.should_not == :parameter
-    end
-
-    it "本番用に、GREE用の初期化が正しく行えること(セッションON)" do
-      c = Rails::Application::Configuration.new
-      OpensocialWap::Platform.gree(c) do
-        consumer_key '(consumer_key)'
-        consumer_secret '(consumer_secret)'
-        sandbox false
-        session true
+      it do
+        @config.opensocial_wap.oauth.helper_class.should                 == OpensocialWap::OAuth::Helpers::BasicHelper
+        @config.opensocial_wap.oauth.helper_class.proxy_class.should     == OpensocialWap::OAuth::RequestProxy::OAuthRackRequestProxyForGree
+        @config.opensocial_wap.oauth.helper_class.consumer_key.should    == '(consumer_key)'
+        @config.opensocial_wap.oauth.helper_class.consumer_secret.should == '(consumer_secret)'
+        @config.opensocial_wap.oauth.helper_class.api_endpoint.should    == 'http://os-sb.gree.jp/api/rest/'
+        @config.opensocial_wap.session_id.should_not == :parameter
       end
 
-      c.opensocial_wap.oauth.helper_class.should == OpensocialWap::OAuth::Helpers::BasicHelper
-      c.opensocial_wap.oauth.helper_class.proxy_class.should == OpensocialWap::OAuth::RequestProxy::OAuthRackRequestProxy
-      c.opensocial_wap.oauth.helper_class.consumer_key.should == '(consumer_key)'
-      c.opensocial_wap.oauth.helper_class.consumer_secret.should == '(consumer_secret)'
-      c.opensocial_wap.oauth.helper_class.api_endpoint.should == 'http://os.gree.jp/api/rest/'
-      c.opensocial_wap.url.container_host == 'mgadget.gree.jp'
-      c.opensocial_wap.url.default.should == {:format => :query, :params => { :guid => 'ON' }}
-      c.opensocial_wap.url.redirect.should == {:format => :local}
-      c.opensocial_wap.url.public_path.should == {:format => :local}
-      c.opensocial_wap.session_id.should == :parameter
+      it "opensocial_wap.url は Proc で controller を渡すと request.mobile? の結果で切り替わる" do
+        ctx = Object.new
+        ctx.stub_chain(:request, :mobile?).and_return(true)
+        retval = @config.opensocial_wap.url.call(ctx)
+        retval.default.should == {:format => :query, :params => {:guid => "ON"}}
+
+        ctx = Object.new
+        ctx.stub_chain(:request, :mobile?).and_return(false)
+        retval = @config.opensocial_wap.url.call(ctx)
+        retval.default.should == {:format => :local}
+      end
     end
   end
 end
--- spec/platform/mobage_spec.rb
+++ spec/platform/mobage_spec.rb
@@ -3,50 +3,74 @@ require 'spec_helper'
 
 describe OpensocialWap::Platform do
   describe "mobage" do
-    it "sandbox用に、mobage用の初期化が正しく行えること(セッションOFF)" do
-      c = Rails::Application::Configuration.new
-      OpensocialWap::Platform.mobage(c) do
-        consumer_key '(consumer_key)'
-        consumer_secret '(consumer_secret)'
-        app_id '9999'
-        sandbox true
-        session false
+    context "sandbox用に、mobage用の初期化が正しく行えること(セッションOFF)" do
+      before do
+        @config = Rails::Application::Configuration.new
+        OpensocialWap::Platform.mobage(@config) do
+          consumer_key '(consumer_key)'
+          consumer_secret '(consumer_secret)'
+          app_id '9999'
+          sandbox true
+          session false
+        end
       end
 
-      c.opensocial_wap.oauth.helper_class.should == OpensocialWap::OAuth::Helpers::MobageHelper
-      c.opensocial_wap.oauth.helper_class.proxy_class.should == OpensocialWap::OAuth::RequestProxy::OAuthRackRequestProxy
-      c.opensocial_wap.oauth.helper_class.consumer_key.should == '(consumer_key)'
-      c.opensocial_wap.oauth.helper_class.consumer_secret.should == '(consumer_secret)'
-      c.opensocial_wap.oauth.helper_class.api_endpoint.should == 'http://sb.app.mbga.jp/api/restful/v1/'
-      c.opensocial_wap.oauth.helper_class.app_id.should == '9999'
-      c.opensocial_wap.url.container_host 'sb.pf.mbga.jp'
-      c.opensocial_wap.url.default.should == {:format => :query, :params => { :guid => 'ON' }}
-      c.opensocial_wap.url.redirect.should == {:format => :local}
-      c.opensocial_wap.url.public_path.should == {:format => :local}
-      c.opensocial_wap.session_id.should_not == :parameter
+      it do
+        @config.opensocial_wap.oauth.helper_class.should == OpensocialWap::OAuth::Helpers::MobageHelper
+        @config.opensocial_wap.oauth.helper_class.proxy_class.should == OpensocialWap::OAuth::RequestProxy::OAuthRackRequestProxy
+        @config.opensocial_wap.oauth.helper_class.consumer_key.should == '(consumer_key)'
+        @config.opensocial_wap.oauth.helper_class.consumer_secret.should == '(consumer_secret)'
+        @config.opensocial_wap.oauth.helper_class.api_endpoint.should == 'http://sb.app.mbga.jp/api/restful/v1/'
+        @config.opensocial_wap.oauth.helper_class.app_id.should == '9999'
+        @config.opensocial_wap.session_id.should_not == :parameter
+      end
+
+      it "opensocial_wap.url は Proc で controller を渡すと request.mobile? の結果で切り替わる" do
+        ctx = Object.new
+        ctx.stub_chain(:request, :mobile?).and_return(true)
+        retval = @config.opensocial_wap.url.call(ctx)
+        retval.default.should == {:format => :query, :params => {:guid => "ON"}}
+
+        ctx = Object.new
+        ctx.stub_chain(:request, :mobile?).and_return(false)
+        retval = @config.opensocial_wap.url.call(ctx)
+        retval.default.should == {:format => :query, :params => {:guid => "ON"}}
+      end
     end
 
-    it "本番用に、mobage用の初期化が正しく行えること(セッションON)" do
-      c = Rails::Application::Configuration.new
-      OpensocialWap::Platform.mobage(c) do
-        consumer_key '(consumer_key)'
-        consumer_secret '(consumer_secret)'
-        app_id '9999'
-        sandbox false
-        session true
+    context "本番用に、mobage用の初期化が正しく行えること(セッションON)" do
+      before do
+        @config = Rails::Application::Configuration.new
+        OpensocialWap::Platform.mobage(@config) do
+          consumer_key '(consumer_key)'
+          consumer_secret '(consumer_secret)'
+          app_id '9999'
+          sandbox false
+          session true
+        end
       end
 
-      c.opensocial_wap.oauth.helper_class.should == OpensocialWap::OAuth::Helpers::MobageHelper
-      c.opensocial_wap.oauth.helper_class.proxy_class.should == OpensocialWap::OAuth::RequestProxy::OAuthRackRequestProxy
-      c.opensocial_wap.oauth.helper_class.consumer_key.should == '(consumer_key)'
-      c.opensocial_wap.oauth.helper_class.consumer_secret.should == '(consumer_secret)'
-      c.opensocial_wap.oauth.helper_class.api_endpoint.should == 'http://app.mbga.jp/api/restful/v1/'
-      c.opensocial_wap.oauth.helper_class.app_id.should == '9999'
-      c.opensocial_wap.url.container_host 'pf.mbga.jp'
-      c.opensocial_wap.url.default.should == {:format => :query, :params => { :guid => 'ON' }}
-      c.opensocial_wap.url.redirect.should == {:format => :local}
-      c.opensocial_wap.url.public_path.should == {:format => :local}
-      c.opensocial_wap.session_id.should == :parameter
+      it do
+        @config.opensocial_wap.oauth.helper_class.should == OpensocialWap::OAuth::Helpers::MobageHelper
+        @config.opensocial_wap.oauth.helper_class.proxy_class.should == OpensocialWap::OAuth::RequestProxy::OAuthRackRequestProxy
+        @config.opensocial_wap.oauth.helper_class.consumer_key.should == '(consumer_key)'
+        @config.opensocial_wap.oauth.helper_class.consumer_secret.should == '(consumer_secret)'
+        @config.opensocial_wap.oauth.helper_class.api_endpoint.should == 'http://app.mbga.jp/api/restful/v1/'
+        @config.opensocial_wap.oauth.helper_class.app_id.should == '9999'
+        @config.opensocial_wap.session_id.should == :parameter
+      end
+
+      it "opensocial_wap.url は Proc で controller を渡すと request.mobile? の結果で切り替わる" do
+        ctx = Object.new
+        ctx.stub_chain(:request, :mobile?).and_return(true)
+        retval = @config.opensocial_wap.url.call(ctx)
+        retval.default.should == {:format => :query, :params => {:guid => "ON"}}
+
+        ctx = Object.new
+        ctx.stub_chain(:request, :mobile?).and_return(false)
+        retval = @config.opensocial_wap.url.call(ctx)
+        retval.default.should == {:format => :query, :params => {:guid => "ON"}}
+      end
     end
   end
 end
