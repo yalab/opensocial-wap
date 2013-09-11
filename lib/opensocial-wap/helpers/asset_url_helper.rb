a2669cc1192781cc091548279efc50ac0af63cf4
t1732 <oo1732oo@gmail.com>
2013-09-11 16:46:42 +0900

    Updated asset_hostが指定されている場合にはcompute_asset_hostの拡張処理を行わないようにする

--- lib/opensocial-wap/helpers/asset_url_helper.rb
+++ lib/opensocial-wap/helpers/asset_url_helper.rb
@@ -10,17 +10,22 @@ module OpensocialWap
       # compute_asset_path を上書き.
       # 初期化時に、opensocial_wap[:url] でURL形式が指定されていれば、パスを
       # OpenSocial 用のものに書き換える.
-      def compute_asset_path(source, options = {})
+      def compute_asset_path(source = "", options = {})
         path = super
-        if default_url_settings
-          # public_path で指定されているオプションを使用する.
-          if default_url_settings.kind_of? Proc
-            url_settings = default_url_settings.call(self).public_path
-          else
-            url_settings = default_url_settings.public_path
+        if defined? config.asset_host && config.asset_host.present?
+          # asset_hostが設定されている場合は処理しない
+        else
+          if default_url_settings
+            # public_path で指定されているオプションを使用する.
+            if default_url_settings.kind_of? Proc
+              url_settings = default_url_settings.call(self).public_path
+            else
+              url_settings = default_url_settings.public_path
+            end
+            path = url_for(path, url_settings)
           end
-          path = url_for(path, url_settings)
         end
+
         path
       end
 
