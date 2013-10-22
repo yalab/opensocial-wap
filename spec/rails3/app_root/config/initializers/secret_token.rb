8029b534b399abb7c9cf7f4379a1ed2771d47212
yalab <rudeboyjet@gmail.com>
2013-10-22 15:42:15 +0900

    spec upgrade to rails 4.0.0.

--- spec/rails3/app_root/config/initializers/secret_token.rb
+++ spec/rails3/app_root/config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-AppRoot::Application.config.secret_token = 'a8d1e514522acf335cbc97e570659973139c696e940d8040839841add8897e800f23b99d16f6b1d0a42f4b5ccfbe06b427c48ba210153a904b4507a19e91353e'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppRoot::Application.config.secret_key_base = '398abf3bb599b98175070fa8e24caa171312771d01df1107e688c4926bbbcec6197bd08937bd81bdf79f218c2ba546904f3a835a7415fdfa6d2d5befa0b22ced'
--- spec/rails3/app_root/config/initializers/session_store.rb
+++ spec/rails3/app_root/config/initializers/session_store.rb
@@ -1,5 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-# User active_record_store to test opensocial_wap_sid.
-AppRoot::Application.config.session_store :active_record_store
-
+AppRoot::Application.config.session_store :cookie_store, key: '_app_root_session'
